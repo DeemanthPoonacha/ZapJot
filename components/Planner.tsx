9ae81f479725987e36ab418693f9a7cafb678f76
deemanth <deemanth.poonacha@ikara.ai>
2025-02-27 14:50:44 +0530

    Planner tab synced with url params

--- components/Planner.tsx
+++ components/Planner.tsx
@@ -1,22 +1,39 @@
 "use client";
 
-import { useState } from "react";
-import { PageHeader } from "@/components/page-header";
+import { useCallback, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import EventsList from "./events/EventsList";
 import GoalsList from "./goals/GoalsList";
 import ItinerariesList from "./itineraries/ItinerariesList";
 import TasksList from "./tasks/TasksList";
 import { Calendar } from "./ui/calendar";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function PlannerPage() {
-  const [activeTab, setActiveTab] = useState("tasks");
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") || "tasks";
+  const [activeTab, setActiveTab] = useState(tab);
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
 
+      return params.toString();
+    },
+    [searchParams]
+  );
   return (
     <>
       <Tabs
         value={activeTab}
-        onValueChange={setActiveTab}
+        onValueChange={(tab) => {
+          setActiveTab(tab);
+          router.push(pathname + "?" + createQueryString("tab", tab));
+        }}
         className="w-full p-4"
       >
         <TabsList className="grid w-full grid-cols-4 bg-muted/50">
